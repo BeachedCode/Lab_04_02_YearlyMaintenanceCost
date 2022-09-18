d66ef3bab8dc115cfbeab490145446ea4b7b3806
Ethan Beach <Beached@mail.uc.edu>
2022-09-18 19:11:04 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double homeWinter = 300.60;
+        double homeSpring = 344.40;
+        double homeSummer = 400.50;
+        double homeAutumn = 300.50;
+        double homeTotal = homeWinter + homeSpring + homeSummer + homeAutumn;
+
+        System.out.println("Yearly home maintenance cost is: " + homeTotal);
+
+
+    }
+}
